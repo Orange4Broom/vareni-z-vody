44b4a9b4d4d1257e0e00e561ca0d834543bc351f
Orange4Broom <orang4broom@gmail.com>
2024-03-04 14:31:04 +0100

    FE: Update Navigation component with active link styling and burger menu

--- src/Components/Blocks/Navigation/Navigation.tsx
+++ src/Components/Blocks/Navigation/Navigation.tsx
@@ -1,21 +1,35 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import './navigation.scss';
+import { Icon } from '@Components/Elements/Icon/Icon';
 
 export const Navigation = () => {
+  const currentLocation = useLocation();
   return (
     <nav className="navigation">
-      <h1 className="navigation__logo">VAŘENÍ Z VODY</h1>
+      <h1 className="navigation__logo">Vaření z vody</h1>
       <div className="navigation__menu">
-        <Link className="navigation__menu__link" to="/">
+        <Link
+          className={`navigation__menu__link${currentLocation.pathname === '/' ? '--active' : ''}`}
+          to="/"
+        >
           MENU
         </Link>
-        <Link className="navigation__menu__link" to="/rate">
+        <Link
+          className={`navigation__menu__link${currentLocation.pathname === '/rate' ? '--active' : ''}`}
+          to="/rate"
+        >
           HODNOCENÍ
         </Link>
-        <Link className="navigation__menu__link" to="/order">
+        <Link
+          className={`navigation__menu__link${currentLocation.pathname === '/order' ? '--active' : ''}`}
+          to="/order"
+        >
           OBJEDNÁNÍ
         </Link>
       </div>
+      <button className="navigation__burger-menu">
+        <Icon name="bars" type="fas" />
+      </button>
     </nav>
   );
 };
